fa45c703b3968e4522d7dd4776cb2914b3a9c5c1
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-03-18 23:57:28 +0530

    add github token for rate limit

--- src/open-router/tools/github.js
+++ src/open-router/tools/github.js
@@ -1,11 +1,22 @@
 export const getUserProfile = async (username) => {
-  const res = await fetch(`https://api.github.com/users/${username}`);
+  const res = await fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authoriztion: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
+      Accept: "application/vnd.github+json",
+    },
+  });
   return res.json();
 };
 
 export const getRepositories = async (username) => {
   const res = await fetch(
     `https://api.github.com/users/${username}/repos`,
+    {
+      headers: {
+        Authoriztion: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
+        Accept: "application/vnd.github+json",
+      },
+    },
   );
   return res.json();
 };
@@ -13,6 +24,12 @@ export const getRepositories = async (username) => {
 export const getLanguages = async (username, repo) => {
   const res = await fetch(
     `https://api.github.com/repos/${username}/${repo}/languages`,
+    {
+      headers: {
+        Authoriztion: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
+        Accept: "application/vnd.github+json",
+      },
+    },
   );
   return res.json();
 };
@@ -20,6 +37,12 @@ export const getLanguages = async (username, repo) => {
 export const getStats = async (username) => {
   const res = await fetch(
     `https://api.github.com/users/${username}/events`,
+    {
+      headers: {
+        Authoriztion: `Bearer ${Deno.env.get("GITHUB_TOKEN")}`,
+        Accept: "application/vnd.github+json",
+      },
+    },
   );
   return res.json();
 };
