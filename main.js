c62c2a9602e7314fca3b70006d511f5f7e7808d9
Sagnik Ghosh <sagnikghosh@Sagniks-MacBook-Pro.local>
2026-03-18 23:25:29 +0530

    Adds docker

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 import { createApp } from "./src/app.js";
 
 const main = () => {
+  const PORT = Deno.env.get("PORT") || 8000;
   const app = createApp();
-  Deno.serve(app.fetch);
+  Deno.serve({ port: PORT }, app.fetch);
 };
 
 main();
